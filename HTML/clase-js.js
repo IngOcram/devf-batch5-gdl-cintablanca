88670652b9e732665f99ff895f8865971c522733
Marco Flores <marco.flores85@hotmail.com>
2018-12-03 19:17:15 -0600

    js

---
+++ HTML/clase-js.js
@@ -0,0 +1,52 @@
+console.log('ya cargo el js');
+
+console.error('ya valio queso');
+
+// comentrio en line
+/*
+otro
+comentario
+*/
+
+var edad=16;
+debugger;
+if(edad>=18){
+    console.log ('tiene peleas en la coliseo');
+    if (edad>=18 & edad<=24)
+    console.log('estas en la universidad') 
+    else{
+        if(edad>=25 & edad<=28){ 
+            console.log('posgrado')
+        }else{
+            if(edad>=29 & edad<=33){
+                console.log('Adulto Joven')  
+            }else{
+                if(edad>=34 & edad<=40){
+                    console.log('Chavo Ruco') 
+            }else{
+                if(edad>=41 & edad<=47){
+                    console.log('Señor Crisis')
+                }else{
+                    if(edad>=48 & edad<=60){
+                        console.log('Viejo Lesbiano')
+                    }
+                }
+            }
+    }
+
+
+}else{
+    console.log('estas morro');
+    if (edad>=6 & edad<=12){
+        console.log('estas muy morro que carajos estas haciendo aqui')
+    }else{
+        if (edad>=13 & edad<=15){
+            console.log ('todavia estas morro')
+        }else{
+            if(edad>=16 & edad<=17){
+                console.log('ya estas cerca del acceso')
+            }
+        } 
+    }
+}
+
