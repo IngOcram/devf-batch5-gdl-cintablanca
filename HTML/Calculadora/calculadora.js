afcecf001b5ccd88d6ac9ebe75f734da8e4035e8
Marco Flores <marco.flores85@hotmail.com>
2018-12-07 21:59:30 -0600

    calculadora viernes

---
+++ HTML/Calculadora/calculadora.js
@@ -0,0 +1,19 @@
+function sumarNumeros (x1,x2){
+    return (Number(x1) + Number(x2));
+}
+function restarNumero (x1,x2){
+    return (Number(x1) - Number(x2));
+}
+function multiplicarNumero (x1,x2){
+    return (Number(x1) * Number(x2));
+}
+function dividirNumero (x1,x2){
+    return (Number(x1) / Number(x2));
+}
+
+function numeros (){
+    
+}
+
+var x1 = document.getElementById("x1").value;
+var x2 = document.getElementById("x2").value;
