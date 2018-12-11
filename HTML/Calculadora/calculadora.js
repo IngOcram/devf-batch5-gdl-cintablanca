f9b51cc35889bc3d529c71fb639f0c52cee158fd
Marco Flores <marco.flores85@hotmail.com>
2018-12-10 21:51:41 -0600

    modificaciones calculadora

--- HTML/Calculadora/calculadora.js
+++ HTML/Calculadora/calculadora.js
@@ -1,15 +1,15 @@
-var  operandoa;
-var  operandob;
-var  operacion;
+var operandoa;
+var operandob;
+var operacion;
 
-function calc (){
-    var resultado = document.getElementsById("resultado");
-    var reset = document.getElementsById("reset");
-    var suma = document.getElementsById("suma");
-    var resta = document.getElementsById("resta");
-    var multiplicacion = document.getElementsById("multiplicacion");
-    var division = document.getElementsById("division");
-    var igual = document.getElementsById("igual");
+function calc() {
+    var pantalla = document.getElementById("pantalla");
+    var reset = document.getElementById("reset");
+    var suma = document.getElementById("suma");
+    var resta = document.getElementById("resta");
+    var multiplicacion = document.getElementById("multiplicacion");
+    var division = document.getElementById("division");
+    var igual = document.getElementById("igual");
     var uno = document.getElementById("uno");
     var dos = document.getElementById("dos");
     var tres = document.getElementById("tres");
@@ -21,119 +21,127 @@ function calc (){
     var nueve = document.getElementById("nueve");
     var cero = document.getElementById("cero");
     var punto = document.getElementById("punto");
-}
-uno.onclick = function(e){
-    resultado.textContent = resultado.textContent + "1";
+    var raiz = document.getElementById("raiz");
 
-}
+    uno.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "1";
 
-dos.onclick = function(e){
-    resultado.textContent = resultado.textContent + "2";
+    }
 
-}
+    dos.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "2";
 
-tres.onclick = function(e){
-    resultado.textContent = resultado.textContent + "3";
+    }
 
-}
+    tres.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "3";
 
-cuatro.onclick = function(e){
-    resultado.textContent = resultado.textContent + "4";
+    }
 
-}
+    cuatro.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "4";
 
-cinco.onclick = function(e){
-    resultado.textContent = resultado.textContent + "5";
+    }
 
-}
+    cinco.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "5";
 
-seis.onclick = function(e){
-    resultado.textContent = resultado.textContent + "6";
+    }
 
-}
+    seis.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "6";
 
-siete.onclick = function(e){
-    resultado.textContent = resultado.textContent + "7";
+    }
 
-}
+    siete.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "7";
 
-ocho.onclick = function(e){
-    resultado.textContent = resultado.textContent + "8";
+    }
 
-}
+    ocho.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "8";
 
-nueve.onclick = function(e){
-    resultado.textContent = resultado.textContent + "9";
+    }
 
-}
+    nueve.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "9";
 
-cero.onclick = function(e){
-    resultado.textContent = resultado.textContent + "0";
+    }
 
-}
+    cero.onclick = function (e) {
+        pantalla.textContent = pantalla.textContent + "0";
 
-reset.onclick = function(e){
-    resetear();
+    }
 
-}
+    punto.onclick = function (e){
+        pantalla.textContent = pantalla.textContent + ".";
+    }
 
-suma.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "+";
-    limpiar();
-}
+    reset.onclick = function (e) {
+        resetear();
 
-resta.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "-";
-    limpiar();
-}
+    }
 
-multiplicacion.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "*";
-    limpiar();
-}
+    suma.onclick = function (e) {
+        operandoa = pantalla.textContent;
+        operacion = "+";
+        limpiar();
+    }
 
-division.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "/";
-    limpiar();
-}
+    resta.onclick = function (e) {
+        operandoa = pantalla.textContent;
+        operacion = "-";
+        limpiar();
+    }
 
-igual.onclick = function(e){
-    operandob = resultado.textContent;
-    resolver();
+    multiplicacion.onclick = function (e) {
+        operandoa = pantalla.textContent;
+        operacion = "*";
+        limpiar();
+    }
+
+    division.onclick = function (e) {
+        operandoa = pantalla.textContent;
+        operacion = "/";
+        limpiar();
+    }
+
+    igual.onclick = function (e) {
+        operandob = pantalla.textContent;
+        resolver();
+    }
+
 }
 
 function limpiar() {
-    resultado.textContent = "";
+    pantalla.textContent = "";
 
 }
 
-function resetear(){
-    resultado.textContent = "";
+function resetear() {
+    pantalla.textContent = "";
     operandoa = 0;
     operandob = 0;
     operacion = "";
 }
 
-function resolver(){
+function resolver() {
     var res = 0;
-    switch(operacion){
-      case "+":
-        res = parseFloat(operandoa) + parseFloat(operandob);
-        break;
-      case "-":
-          res = parseFloat(operandoa) - parseFloat(operandob);
-          break;
-      case "*":
-        res = parseFloat(operandoa) * parseFloat(operandob);
-        break;
-      case "/":
-        res = parseFloat(operandoa) / parseFloat(operandob);
-        break;
+    switch (operacion) {
+        case "+":
+            res = parseFloat(operandoa) + parseFloat(operandob);
+            break;
+        case "-":
+            res = parseFloat(operandoa) - parseFloat(operandob);
+            break;
+        case "*":
+            res = parseFloat(operandoa) * parseFloat(operandob);
+            break;
+        case "/":
+            res = parseFloat(operandoa) / parseFloat(operandob);
+            break;
     }
     resetear();
-    resultado.textContent = res;
-  }
+    pantalla.textContent = res;
+    operandoa = res;
+}
